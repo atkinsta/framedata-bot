63daf0b54f1936ebbe34677b51330bfb9e839d2e
atkinsta <atkinstaa@gmail.com>
2018-10-08 17:59:39 -0400

    First working prototype up and running

--- bot.js
+++ bot.js
@@ -4,12 +4,14 @@ const Snoowrap = require("snoowrap");
 const Snoostorm = require("snoostorm");
 
 const wrap = new Snoowrap({
-    userAgent = "framedata-bot",
+    userAgent: "framedata-bot",
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     username: process.env.REDDIT_USER,
     password: process.env.REDDIT_PASS
 });
+
+wrap.config({requestDelay: 1000, warnings: false, continueAfterRatelimitError: true})
 
 const storm = new Snoostorm(wrap);
 
@@ -21,5 +23,12 @@ const stream = {
 const comments = storm.CommentStream(stream);
 
 comments.on("comment", comment => {
-    console.log("comment");
+    if (comment.body.includes("!frames")) {
+        let body = comment.body;
+        let start = "!frames";
+        let reg = new RegExp(start + "\\s(\\w+)");
+        console.log(body.match(reg));
+        let term = body.match(reg);
+        comment.reply(`[${term[1]}'s Frame Data](http://rbnorway.org/${term[1]}-t7-frames/)\n\n\n^^This ^^is ^^a ^^bot. ^^To ^^learn ^^more ^^about ^^usage ^^visit [^^my ^^github](http://www.github.com/atkinsta/framedata-bot)`)
+    }
 });
