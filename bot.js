5d1babf08691e6b4eab27067c890b19c904d71c0
atkinsta <atkinstaa@gmail.com>
2018-10-08 16:48:00 -0400

    Set up comment stream

--- bot.js
+++ bot.js
@@ -1,2 +1,25 @@
 require("dotenv").config();
 
+const Snoowrap = require("snoowrap");
+const Snoostorm = require("snoostorm");
+
+const wrap = new Snoowrap({
+    userAgent = "framedata-bot",
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    username: process.env.REDDIT_USER,
+    password: process.env.REDDIT_PASS
+});
+
+const storm = new Snoostorm(wrap);
+
+const stream = {
+    subreddit: "Tekken",
+    results: 25
+}
+
+const comments = storm.CommentStream(stream);
+
+comments.on("comment", comment => {
+    console.log("comment");
+});
